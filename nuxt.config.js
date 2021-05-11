8a6656b43b2f7218335539c4abf80595457daffb
Yoichi Kobayashi <info@tplh.net>
2021-05-11 19:43:21 +0900

    added normalize.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    'normalize.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
