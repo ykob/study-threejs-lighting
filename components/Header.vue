57e115dc8c584c2c2c05e3dbd54a4c6180f1066c
Yoichi Kobayashi <info@tplh.net>
2021-05-20 14:31:16 +0900

    udpate styles.

--- components/Header.vue
+++ components/Header.vue
@@ -15,8 +15,26 @@ export default {}
 <style lang="scss" scoped>
 .header {
   position: fixed;
-  top: 5%;
-  left: 5%;
+  padding-bottom: 0;
+  bottom: 0;
+  left: 0;
   z-index: z(header);
+  letter-spacing: 0.1em;
+  @include l-pc {
+    padding: 40px;
+  }
+  @include l-mobile {
+    padding: 20px;
+  }
+  h1 {
+    margin-top: 0;
+    margin-bottom: 12px;
+    font-size: 18px;
+  }
+  p {
+    line-height: 1.5;
+    margin: 0;
+    font-size: 11px;
+  }
 }
 </style>
