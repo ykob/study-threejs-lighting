b68bd69de6d17c0048ff8043e2a4cb612dc72f77
Yoichi Kobayashi <info@tplh.net>
2021-05-23 21:50:06 +0900

    added icon components.

---
+++ components/icon/Cube.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+IconBase(
+  iconName = 'cube'
+  :size = 'size'
+  :fill = 'fill'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script>
+import { mdiCube } from '@mdi/js'
+
+export default {
+  props: {
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+    fill: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+  computed: {
+    d() {
+      return mdiCube
+    },
+  },
+}
+</script>
---
+++ components/icon/Play.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+IconBase(
+  iconName = 'play'
+  :size = 'size'
+  :fill = 'fill'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script>
+import { mdiPlay } from '@mdi/js'
+
+export default {
+  props: {
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+    fill: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+  computed: {
+    d() {
+      return mdiPlay
+    },
+  },
+}
+</script>
