3c2dd2eb929be5ffb124bda1565daa86e61288a9
Yoichi Kobayashi <info@tplh.net>
2021-05-25 23:52:56 +0900

    added the restore icon component.

---
+++ components/icon/Restore.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+IconBase(
+  iconName = 'restore'
+  :size = 'size'
+  :fill = 'fill'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script>
+import { mdiRestore } from '@mdi/js'
+
+export default {
+  props: {
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+    fill: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+  computed: {
+    d() {
+      return mdiRestore
+    },
+  },
+}
+</script>
