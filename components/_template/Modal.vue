409ae03e4735a04d0b6a103e5fa766b12af5423f
Yoichi Kobayashi <info@tplh.net>
2021-06-14 21:51:08 +0900

    added the styles of the help modal.

--- components/_template/Modal.vue
+++ components/_template/Modal.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
 .modal
+  .modal__content
+    ModalHelp
+  .modal__bg
 </template>
 
 <script>
@@ -8,6 +11,35 @@ export default {}
 
 <style lang="scss" scoped>
 .modal {
+  width: 100%;
+  height: 100%;
   pointer-events: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: z(modal);
+  &__content {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-right: 24px;
+    padding-left: 24px;
+  }
+  &__bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    pointer-events: auto;
+    background-color: rgba(#202, 0.6);
+  }
 }
 </style>
---
+++ components/modal/ModalHelp.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.modal-help
+  .modal-help__heading
+    |About This Page
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.modal-help {
+  width: 100%;
+  max-width: 720px;
+  height: 100%;
+  max-height: 480px;
+  box-sizing: border-box;
+  pointer-events: auto;
+  border-radius: 8px;
+  background-color: #111;
+  @include l-pc {
+    padding: 64px 48px;
+  }
+  @include l-mobile {
+    padding: 32px 24px;
+  }
+  &__heading {
+    text-align: center;
+    letter-spacing: 0.1em;
+  }
+}
+</style>
