f517c94625f1e341bff239f3090e50edf166d9e6
Yoichi Kobayashi <info@tplh.net>
2021-06-14 21:59:52 +0900

    added  user-scalable=no to viewport.

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
