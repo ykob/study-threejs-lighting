e95fec3eaa5c113bfba1ce6f64013b2c7376ebdd
Yoichi Kobayashi <info@tplh.net>
2021-09-02 21:48:47 +0900

    editted description.

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'It\'s a study record of 3D graphic lighting with the shaders that I created by quoting from three.js.' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
